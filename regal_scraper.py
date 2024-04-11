bdebc0ea7597939b553ce1bb10cd625f79c11aac
Ryan <ryan.mai@mail.com>
2024-04-10 17:40:58 -0700

    initial commit

---
+++ regal_scraper.py
@@ -0,0 +1,8 @@
+/*
+ * Filename: /Users/Nic/Documents/Python Projects/regal_scraper-1/regal_scraper
+ * Path: /Users/Nic/Documents/Python Projects/regal_scraper-1
+ * Created Date: Wednesday, April 10th 2024, 11:21:20 am
+ * Author: Ryan2
+ * 
+ * Copyright (c) 2024 Your Company
+ */
---
+++ sql/schema_definitions.py
@@ -0,0 +1,18 @@
+theatres_schema ="""CREATE TABLE theatres(
+	name TEXT,
+	path_name TEXT,
+	theatre_code TEXT,
+	message_start TEXT,
+	message_end TEXT,
+	message_title TEXT,
+	theatre_info_message TEXT,
+	address TEXT,
+	city TEXT,
+	state TEXT,
+	postal_code TEXT,
+	latitude REAL,
+	longitude REAL,
+	phone_number TEXT,
+	pwp_availability INTEGER,
+	iana_timezone TEXT,
+    """
