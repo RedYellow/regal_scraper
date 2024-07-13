160e7ea7199b1ff66791d36826cda6ca7689fb99
Ryan Mai <ryan.mai@mail.com>
2024-07-13 01:50:20 -0700

    working showings getter and dataclass wrappers

--- regal_scraper/Movie.py
+++ regal_scraper/Movie.py
@@ -2,7 +2,32 @@ from dataclasses import dataclass
 from datetime import datetime
 
 @dataclass
+class Showing:
+	Auditorium: int
+	PerformanceId: int
+	PerformanceAttributes: list[str]
+	PerformanceGroup: str
+	CalendarShowTime: str
+	UtcShowTime: str
+	UnixTime: int
+	StopSales : bool
+
+	def __post_init__(self):
+		self.utc_showtime = datetime.fromisoformat(self.UtcShowTime)
+		self.calendar_showtime = datetime.fromisoformat(self.CalendarShowTime)
+
+
+@dataclass
 class Movie:
 	title: str
-	utc_date: datetime
+	performances: dict
+	
+	def __post_init__(self):
+		self.performances = [Showing(**_) for _ in self.performances]
+
+	# utc_date: datetime
+	# def __init__(movie_dict):
+	# 	# dict is from the API response
+	# 	title = movie_dict["Title"]
+	# 	performances
 	
--- regal_scraper/regal_scraper.py
+++ regal_scraper/regal_scraper.py
@@ -1,7 +1,12 @@
 
+from datetime import datetime
 from database import Database
 import requests
 from urllib.parse import urljoin, urlencode
+
+from Movie import Movie
+
+# from Theatre import Theatre
 
 class RegalAPI:
 	def __init__(self):
@@ -11,19 +16,31 @@ class RegalAPI:
 		self.base_url = "https://www.regmovies.com"
 
 	def set_local_theatre(self, name):
-		self.local_theatre_path_name = self.db.get_theatre_code_from_name(name)
-		self.local_theatre = name
+		self.local_theatre = name #TODO: make this a Theatre object
+		self.local_theatre_code =  self.db.get_theatre_code_from_name(name)
+		self.local_theatre_path_name = self.db.get_theatre_path_name_from_name(name)
 
 
-	def get_showings(self, theatre_code):
-		url = self.build_url(self.base_url, "")
+	def get_showings(self, date = datetime.now()):
+		# url = self.build_url(self.base_url, "")
+		url = f"https://www.regmovies.com/api/getShowtimes?theatres={self.local_theatre_code}&date={date.strftime('%m-%d-%Y')}&hoCode=&ignoreCache=false&moviesOnly=false"
+		response = requests.get(url = url, headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0"})
+		# return response.json()['shows'][0]["Film"]
+	
+		return [Movie(film["Title"], film["Performances"]) for film in response.json()['shows'][0]["Film"]]
 		
 		
-		#  
+		 
+	
 
 	def build_url():
 		...
 
 
 if __name__ == "__main__":
+	client = RegalAPI()
+	client.set_local_theatre("Regal Edwards Mira Mesa")
+	showings = client.get_showings(datetime.now().date())
 	print("zoop")
+	
+	
